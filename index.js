d9d4f53f18ef691492c37cd8f08d560d51dfefe3
Max Hrechko <mhrechko@blueskyelearn.com>
2023-05-12 11:44:13 +0300

    Add seeding

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { CSVLoader } from 'langchain/document_loaders/fs/csv';
+import { JSONLoader } from "langchain/document_loaders/fs/json";
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import http from 'http';
@@ -10,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 const loader = new CSVLoader('./data.csv');
 const docs = await loader.load();
-const vectorStore = await MemoryVectorStore.fromDocuments(
+let vectorStore = await MemoryVectorStore.fromDocuments(
   docs,
   new OpenAIEmbeddings()
 );
@@ -22,7 +23,7 @@ const getSimilarDocuments = async (questions) => {
       const pageContent = result[0].pageContent.split('\n')[0]
       const metadata = {
         question: question,
-        item: pageContent.split(': ')[1]
+        item: pageContent.split('|')[0]
       };
       return metadata;
     })
@@ -30,23 +31,75 @@ const getSimilarDocuments = async (questions) => {
   return results;
 };
 
+const seedDocuments = async (blob) => {
+  const loader = new JSONLoader(blob, ["/description"]);
+  const docs = await loader.load();
+  vectorStore = await MemoryVectorStore.fromDocuments(
+    docs,
+    new OpenAIEmbeddings()
+  );
+  return vectorStore.memoryVectors;
+};
+
+const handleAsk = async (req, res) => {
+  let body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const params = JSON.parse(body);
+
+    getSimilarDocuments(params.questions, vectorStore).then((result) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ status: 'success', data: result }));
+    });
+  });
+}
+
+const handleSeed = async (req, res) => {
+  let body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on('end', () => {
+    const json = JSON.parse(body);
+    for (let key in json) {
+      if (json.hasOwnProperty(key)) {
+        let item = json[key];
+        item.description = `${item.url}|${item.description}`;
+      }
+    }
+    const jsonString = JSON.stringify(json);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+
+    seedDocuments(blob).then((result) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ status: 'success', data: result }));
+    });
+  });
+}
+
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
-    let body = '';
-
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      const params = JSON.parse(body);
-
-      getSimilarDocuments(params.questions).then((result) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ status: 'success', data: result }));
-      });
-    });
+    switch (req.url) {
+      case '/ask':
+        handleAsk(req, res);
+        break;
+      case '/seed':
+        handleSeed(req, res);
+        break;
+      default:
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Endpoint not found');
+        break;
+    }
   } else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
